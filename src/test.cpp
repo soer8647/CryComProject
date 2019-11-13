5e313ae5a20f62845fd530b0269695905062389a
Shjorty <201505261@post.au.dk>
2019-11-13 14:28:18 +0100

    changed ec

--- src/test.cpp
+++ src/test.cpp
@@ -43,11 +43,17 @@ int main(int argc, char* argv[])
 {
     using namespace CryptoPP;
 
-    Integer p("0xDB7C2ABF62E35E668076BEAD208B");
-    Integer a("0xDB7C2ABF62E35E668076BEAD2088");
-    Integer b("0x659EF8BA043916EEDE8911702B22");
-    Integer x("0x09487239995A5EE76B55F9C2F098");
-    Integer y("0xA89CE5AF8724C0A23E0E0FF77500");
+    //Integer p("0xDB7C2ABF62E35E668076BEAD208B");
+    //Integer a("0xDB7C2ABF62E35E668076BEAD2088");
+    //Integer b("0x659EF8BA043916EEDE8911702B22");
+    //Integer x("0x09487239995A5EE76B55F9C2F098");
+    //Integer y("0xA89CE5AF8724C0A23E0E0FF77500");
+
+    Integer p("0x01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+    Integer a("0x01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC");
+    Integer b("0x0051953EB9618E1C9A1F929A21A0B68540EEA2DA725B99B315F3B8B489918EF109E156193951EC7E937B1652C0BD3BB1BF073573DF883D2C34F1EF451FD46B503F00");
+    Integer x("0x00C6858E06B70404E9CD9E3ECB662395B4429C648139053FB521F828AF606B4D3DBAA14B5E77EFE75928FE1DC127A2FFA8DE3348B3C1856A429BF97E7E31C2E5BD66");
+    Integer y("0x011839296A789A3BC0045C8A5FB42C7D1BD998F54449579B446817AFBD17273E662C97EE72995EF42640C550B9013FAD0761353C7086A272C24088BE94769FD16650");
 
     int c_lst[] = {0, 1, 2, 3, 4};
     std::string msgs[3][5] = {{"besked a0", "besked a1", "besked a2", "besked a3", "besked a4"},
@@ -59,6 +65,7 @@ int main(int argc, char* argv[])
     static int m = 3; //TODO
     byte* bytes[n] = {};
     int size_m = stringsToByte(msgs[0], n, bytes); //TODO
+    std::cout << size_m << std::endl;
 
     ECP ec = ECP(p,a,b);
     Point basePoint = Point(x,y);
@@ -76,13 +83,8 @@ int main(int argc, char* argv[])
     auto t2 = std::chrono::high_resolution_clock::now();
     printResult(m_c, t1, t2, size_m);
 
-    std::cout << "output: ";
-    for(int i=0; i<size_m; i++) {
-      std::cout << m_c[i];
-    }
-    std::cout << " in time: " << duration << std::endl;
 
-    int n = 3;
+    n = 3;
     int k = 2;
     int size_message = 2;
 
